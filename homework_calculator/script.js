bd04486890e0e96d520dbb03c0c879d1d2338c5f
TodorPelivanov <tpelivanov@gmail.com>
2022-01-23 17:47:01 +0100

    calc

---
+++ homework_calculator/script.js
@@ -0,0 +1,23 @@
+let showResult = document.getElementById(`#upperPart2`);
+let add = document.getElementById(`#add`);
+let sustract = document.getElementById(`#sustract`);
+let multiply = document.getElementById(`#multiply`);
+let divide = document.getElementById(`#divide`);
+let backspace = document.getElementById(`#bck`);
+let reset = document.getElementById(`#reset`);
+let percentage = document.getElementById(`#percentage`);
+let negate = document.getElementById(`#negate`);
+let numOne = document.getElementById(`#numberOne`);
+let numTwo = document.getElementById(`#numberTwo`);
+let numThree = document.getElementById(`#numberThree`);
+let calcResult = document.getElementById(`#calcResult`);
+let numFour = document.getElementById(`#numberFour`);
+let numFive = document.getElementById(`#numberFive`);
+let numSix = document.getElementById(`#numberSix`);
+let dot = document.getElementById(`#dot`);
+let numSeven = document.getElementById(`#numberSeven`);
+let numEight = document.getElementById(`#numberEight`);
+let numNine = document.getElementById(`#numberNine`);
+let numZero = document.getElementById(`#numberZero`);
+
+
